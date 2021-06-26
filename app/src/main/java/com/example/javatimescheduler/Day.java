055898c91748c71797e26d6d1d5fa2d2193fd034
matt <mlryckman27@gmail.com>
2021-06-26 16:37:48 -0400

    Started designing fields and methods for Day and ScheduleFile classes.  Created and outline for the Schedule interface, but need to make it conform to Java interface standards before implementing it in ScheduleFile class.

--- app/src/main/java/com/example/javatimescheduler/Day.java
+++ app/src/main/java/com/example/javatimescheduler/Day.java
@@ -1,4 +1,37 @@
 package com.example.javatimescheduler;
 
-public class Day {
+
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Day extends AppCompatActivity {
+
+    private EditText viewStart;     // The start time for the day.
+    private EditText viewEnd;       // The end time for the day.
+
+
+    // Id is of the form R.id.some_xml_label
+    public Day(int startId, int endId) {
+        viewStart = findViewById(startId);
+        viewEnd = findViewById(endId);
+    }
+
+
+    public EditText getViewStart() {
+        return viewStart;
+    }
+
+    public void setViewStart(EditText viewStart) {
+        this.viewStart = viewStart;
+    }
+
+    public EditText getViewEnd() {
+        return viewEnd;
+    }
+
+    public void setViewEnd(EditText viewEnd) {
+        this.viewEnd = viewEnd;
+    }
 }
--- app/src/main/java/com/example/javatimescheduler/ScheduleFile.java
+++ app/src/main/java/com/example/javatimescheduler/ScheduleFile.java
@@ -1,4 +1,19 @@
 package com.example.javatimescheduler;
 
-public class ScheduleFile {
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+
+
+public class ScheduleFile extends AppCompatActivity {
+
+
+    File pathToScheduleFile;
+
+
+    public ScheduleFile() {
+        pathToScheduleFile = new File(getFilesDir() + File.separator + "JTimeSchedule.txt");
+    }
+
 }
