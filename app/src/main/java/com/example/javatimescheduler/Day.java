1507e6482ee642721bcd4f84317ea40b864078cd
mlryckman27 <mlryckman27@gmail.com>
2021-07-07 18:00:54 -0400

    Finished methods for Day class and placed Views for each day's start/end time in axtivity_main layout.

--- app/src/main/java/com/example/javatimescheduler/Day.java
+++ app/src/main/java/com/example/javatimescheduler/Day.java
@@ -1,17 +1,10 @@
 package com.example.javatimescheduler;
 
-
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.Button;
-import android.widget.EditText;
-
 
 public class Day extends androidx.appcompat.widget.AppCompatEditText {
 
-    private int[] hours;       // 0 - 23
-    private int[] minutes;    // 0 - 59
     private String time;
 
     public Day(Context context) {
@@ -22,7 +15,6 @@ public class Day extends androidx.appcompat.widget.AppCompatEditText {
     public Day(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
-        super.setText(time);
     }
 
     public Day(Context context, AttributeSet attrs, int defStyleAttrs) {
@@ -31,27 +23,15 @@ public class Day extends androidx.appcompat.widget.AppCompatEditText {
     }
 
     private void init() {
-        hours = new int[2];
-        minutes = new int[2];
-
-        hours[0] = 0;
-        hours[1] = 0;
-        minutes[0] = 0;
-        minutes[1] = 0;
-
-        time = hours[0] + hours[1] + ":" + minutes[0] + minutes[1];
+        time = "start";
     }
 
-    private setHours(int hours) {
-
+    public void setTime(String time) {
+        super.setText(time);
     }
 
-    private setMinutes(int minutes) {
-
-    }
-
-    public void setTime(int hours, int minutes) {
-
+    public String getTime() {
+        return super.getText().toString();
     }
 
 }
--- app/src/main/java/com/example/javatimescheduler/MainActivity.java
+++ app/src/main/java/com/example/javatimescheduler/MainActivity.java
@@ -1,35 +1,30 @@
 package com.example.javatimescheduler;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
-
 
 public class MainActivity extends AppCompatActivity {
+
+    Day mondayStart, tuesdayStart, wednesdayStart, thursdayStart, fridayStart;
+    Day mondayEnd, tuesdayEnd, wednesdayEnd, thursdayEnd, fridayEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mondayStart = findViewById(R.id.monday_start_edittext);
+        tuesdayStart = findViewById(R.id.tuesday_start_edittext);
+        wednesdayStart = findViewById(R.id.wednesday_start_edittext);
+        thursdayStart = findViewById(R.id.thursday_start_edittext);
+        fridayStart = findViewById(R.id.friday_start_edittext);
+
+        mondayEnd = findViewById(R.id.monday_end_edittext);
+        tuesdayEnd = findViewById(R.id.tuesday_end_edittext);
+        wednesdayEnd = findViewById(R.id.wednesday_end_edittext);
+        thursdayEnd = findViewById(R.id.thursday_end_edittext);
+        fridayEnd = findViewById(R.id.friday_end_edittext);
     }
 
 }
